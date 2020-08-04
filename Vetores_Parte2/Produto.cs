a472f5fe3992b306105bb8a7e9feaf14dab5ff58
Carlos Vieira <cadu.vieira2276@gmail.com>
2020-08-04 19:39:31 -0300

    Projeto sobre vetores

---
+++ Vetores_Parte2/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Vetores_Parte2 {
+    class Produto {        
+        public string Nome { get; set; }
+        private double Preco { get; set; }
+
+        public Produto(string nome, double preco) {
+            Nome = nome;
+            Preco = preco;
+        }
+
+        public double GetSoma() {
+            double soma = 0.0;
+            soma += Preco;
+            return soma;
+        }
+
+    }
+}
---
+++ Vetores_Parte2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores_Parte2 {
+    class Program {
+        public static double preco { get; private set; }
+
+        static void Main(string[] args) {
+            int n;                       
+            n = int.Parse(Console.ReadLine());
+            Produto[] vet = new Produto[n];
+            
+            for (int i = 0; i<n; i++) {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vet[i] = new Produto(nome, preco);
+            }
+
+            double soma = 0.0;
+            for (int i = 0; i < n; i++) {
+                soma += vet[i].GetSoma();                
+                Console.WriteLine("Soma = " + soma.ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+        }
+    }
+}
